32bc2f1b16706507a422a9470c137cd635cf68d1
nadav.grinberg <nadav.grinberg@siemens.com>
2020-02-10 17:25:47 +0200

    Add portal assets & init code

---
+++ Assets/Scripts/Portal.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Portal : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject otherPortal;
+
+    IEnumerator Wait(int n)
+    {
+        yield return new WaitForSeconds(n);
+    }
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name != "basketball") return;
+
+        teleport(collision.gameObject);
+    }
+
+    void teleport(GameObject ball)
+    {
+        ball.transform.position = otherPortal.transform.position;
+        ball.GetComponent<CircleCollider2D>().enabled = false;
+    }
+}
